47f2d2b8f2fd803a738c4a519ef6040e51cda2f6
PARULEKAR Rohan <rohan.s.parulekar@nokia.com>
2016-04-18 12:40:13 -0700

    Committing a master script to do an end to end Rackspace OSA deployment with Nuage

---
+++ nuage_rackspace_deploy.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+echo "Starting an end to end deployment of Rackspace OS with Nuage neutron plugin"
+
+cd /opt/openstack-ansible/playbooks
+sudo openstack-ansible setup-hosts.yml
+sudo openstack-ansible setup-infrastructure.yml
+
+if [ $1 == haproxy ] ; then
+  sudo openstack-ansible haproxy-install.yml
+else
+  echo "HA proxy is not configured will skip running the haproxy install playbook"
+fi
+
+sudo openstack-ansible setup-openstack.yml
+
+echo "Running Nuage VRS playbooks to configure OS compute nodes with Nuage VRS"
+cd /opt/openstack-ansible/nuage-openstack-ansible/nuage_playbooks
+sudo openstack-ansible nuage_all.yml
