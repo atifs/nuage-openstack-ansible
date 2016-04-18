b883713a8b8560df0302752a1d71116ee12aa6b2
PARULEKAR Rohan <rohan.s.parulekar@nokia.com>
2016-04-18 15:15:49 -0700

    Corected working directory for nuage ansible playbooks

--- nuage_rackspace_deploy.sh
+++ nuage_rackspace_deploy.sh
@@ -16,5 +16,5 @@ fi
 sudo openstack-ansible setup-openstack.yml
 
 echo "Running Nuage VRS playbooks to configure OS compute nodes with Nuage VRS"
-cd /opt/openstack-ansible/nuage-openstack-ansible/nuage_playbooks
+cd /opt/nuage-openstack-ansible/nuage_playbooks
 sudo openstack-ansible nuage_all.yml
